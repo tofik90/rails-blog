75e8c04f9013964b9f4b75c93cbe4a46eda37ca0
tofik90 <ru-tofik@yandex.ru>
2019-09-21 16:43:01 +0300

    hz

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+Blog::Application.routes.draw do
+  resources :contacts, only: [:new, :create]
+  resources :articles
+end
+
+
