5541aa27443d63c48b1f97769be4610e929ce662
tofik90 <ru-tofik@yandex.ru>
2019-09-23 15:46:18 +0300

    Update articles create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,18 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+
   def new
   end
 
   def create
+    @articles = Article.all
     @article = Article.new(articles_params)
       if @article.valid?
         @article.save
+        redirect_to @article
       else
         render action: 'new'
     end  
