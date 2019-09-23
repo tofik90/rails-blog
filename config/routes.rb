ff6ee4c96586e053f7ad609489a43d83ea0fd848
tofik90 <ru-tofik@yandex.ru>
2019-09-23 23:07:11 +0300

    Implement pages#about and pages#terms

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get '/terms', to: 'pages#terms'
+  get '/about', to: 'pages#about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
 
 Blog::Application.routes.draw do
   resources :contacts, only: [:new, :create], path_names: { :new => ''}
   resources :articles
 end
+
+
+
 
 
