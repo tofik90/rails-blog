2aa9ef197271fbd4e1e880e9a09f9abc1ea17b0f
tofik90 <ru-tofik@yandex.ru>
2019-09-25 21:17:42 +0300

    Add devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end 
 
 
