2ed7a8d0cb3fffed93cbf6601cac9c20e1828ebe
tofik90 <ru-tofik@yandex.ru>
2019-09-27 18:42:14 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,11 @@
 require "spec_helper"
 
 describe Article do
-   it { should have_many :comments }
-  
+  describe "associations" do 
+    it { should have_many :comments }
+  end
+  describe "validations" do
+    it { should validate_presence_of :title} 
+    it { should validate_presence_of :text}
+  end
 end
