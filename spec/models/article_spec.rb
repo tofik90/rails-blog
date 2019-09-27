84f47f5561bbdd8719b0e5945922ae6be9f1df4a
tofik90 <ru-tofik@yandex.ru>
2019-09-27 19:27:43 +0300

    Update article rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,8 +4,19 @@ describe Article do
   describe "associations" do 
     it { should have_many :comments }
   end
+
   describe "validations" do
     it { should validate_presence_of :title} 
     it { should validate_presence_of :text}
+  end
+
+  describe "#subject" do
+    it "returns the article title" do
+      # Cоздаем объект article хитрым способом
+      article = create(:article, title: 'Lorem Ipsum')
+    
+      #axpect, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
   end
 end
