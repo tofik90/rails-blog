9bada9ac3ff7003a95706c64e35151e7591fd5c9
tofik90 <ru-tofik@yandex.ru>
2019-09-27 18:35:57 +0300

    Add contact model spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
-require 'shoulda/matchers'
+
 
 describe Contact do 
-  it { should validate_presence_of (:email)} 
+  it { should validate_presence_of :email} 
+  it { should validate_presence_of :message}
 end
